e84afe1fc9fc8e6d04e44e195a4fcae8038df6ca
David North <david.north@purplekingdomgames.com>
2018-06-07 13:39:25 +0100

    Caches camera list
    Searching for all cameras in a scene each time a script is enabled or disabled may not be performant (depending on how often you're doing it)
    
    This sacrifices the ability to just add cameras to a scene for the performance boost of not having to search for all cameras on each enable/disable

--- OutlineEffect/Assets/OutlineEffect/Outline.cs
+++ OutlineEffect/Assets/OutlineEffect/Outline.cs
@@ -2,17 +2,17 @@
 //  Copyright (c) 2015 José Guerreiro. All rights reserved.
 //
 //  MIT license, see http://www.opensource.org/licenses/mit-license.php
-//  
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,7 +23,6 @@
 */
 
 using UnityEngine;
-using System.Linq;
 using System.Collections.Generic;
 
 namespace cakeslice
@@ -37,32 +36,37 @@ namespace cakeslice
         public int color;
         public bool eraseRenderer;
 
+        private Camera[] _cameras;
+
         private void Awake()
         {
             Renderer = GetComponent<Renderer>();
+            _cameras = Camera.allCameras;
         }
 
         void OnEnable()
         {
-			IEnumerable<OutlineEffect> effects = Camera.allCameras.AsEnumerable()
-				.Select(c => c.GetComponent<OutlineEffect>())
-				.Where(e => e != null);
-
-			foreach (OutlineEffect effect in effects)
-            {
-                effect.AddOutline(this);
+            foreach (Camera c in _cameras) {
+                if (c != null) {
+                    OutlineEffect e = c.GetComponent<OutlineEffect>();
+                    if (e != null)
+                    {
+                        e.AddOutline(this);
+                    }
+                }
             }
         }
 
         void OnDisable()
         {
-			IEnumerable<OutlineEffect> effects = Camera.allCameras.AsEnumerable()
-				.Select(c => c.GetComponent<OutlineEffect>())
-				.Where(e => e != null);
-
-			foreach (OutlineEffect effect in effects)
-            {
-                effect.RemoveOutline(this);
+			foreach (Camera c in _cameras) {
+                if (c != null) {
+                    OutlineEffect e = c.GetComponent<OutlineEffect>();
+                    if (e != null)
+                    {
+                        e.RemoveOutline(this);
+                    }
+                }
             }
         }
     }
